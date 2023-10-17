49b0e8e1b73f16f65b63c64babe9b9a419dafa50
SanjanaKatti <sanjanakatti16@gmail.com>
2023-10-17 13:16:10 +0530

    json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import FormControl from './FormControl.js';
 import './App.css';
-import './FormControl.js';
+
 function App() {
-  const roptions = [
-    {id:"html", value:"HTML"},
-    {id:"css", value:"CSS"},
-    {id:"java", value:"JAVA"}
-]
-  const ddOptions = [
-    {key:"opt1", label:"Option 1"},
-    {key:"opt2", label:"Option 2"},
-    {key:"opt3", label:"Option 3"},
-  ] 
+  const formObjects = [
+    {
+      "inputType" : "text",
+      "label" : "Full Name",
+    },
+    {
+      "inputType" : "dropdown",
+      "label": "Gender",
+      "options" :[
+        { "key": "opt1", "label": "Male" },
+        { "key": "opt2", "label": "Female" },
+        { "key": "opt3", "label": "Others" },
+      ]
+    },
+    {
+      "inputType" : "submit",
+      "label" : "Submit"
+    },
+  ]
+  
+
   return (
-    
     <form>
-    <FormControl inputType="text" />
-    <FormControl inputType="submit" />
-    <FormControl inputType="time" />
-    <FormControl inputType="radio" roptions={roptions}/>
-    <FormControl inputType="number" />
-    <FormControl inputType="date" />
-    <FormControl inputType="dropdown" ddOptions={ddOptions} />
+      {formObjects.map((formObject,index) => (
+          <FormControl  key={index} inputType={formObject.inputType} label={formObject.label} options={formObject.options} />
+      ))}
     </form>
   );
 }
 
 export default App;
+
+
+
--- src/FormControl.js
+++ src/FormControl.js
@@ -2,16 +2,16 @@ import TextBox from './Elements/TextBox';
 import Submit from './Elements/Submit';
 import Radio from './Elements/Radio';
 import DropDown from './Elements/DropDown';
-function FormControl({inputType,ddOptions,roptions}) {
+function FormControl({inputType,options,label}) {
     switch(inputType)
         {
-            case "text": return <TextBox name="Sanjana" id="UserName" value="hello" placeHolder="Enter your name" maxLength="30" label="TextBox"/>
-            case "submit": return <Submit value="Submit"/>
+            case "text": return <TextBox name="Sanjana" id="UserName" value="hello" placeHolder="Enter your name" maxLength="30" label={label}/>
+            case "submit": return <Submit value="Submit" label={label}/>
             case "time": return <div><input type={inputType}/></div>
-            case "radio": return <Radio name="fav_language" type={inputType} options={roptions}/>
+            case "radio": return <Radio name="fav_language" type={inputType} options={options}/>
             case "number": return <div><input type={inputType}/></div>
             case "date": return <div><input type={inputType}/></div>
-            case "dropdown": return<div><DropDown name="Options" options={ddOptions}/></div>
+            case "dropdown": return<div><DropDown name="Options" options={options}/></div>
             default : return <div>HI</div>
         }
 }
